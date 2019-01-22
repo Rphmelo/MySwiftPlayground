8ce71b0264cee9d7ed41e79b4125badd72e2d7e3
Usuário Convidado <Guest@pa06micro32.local>
2019-01-21 23:03:03 -0200

    Optionals, collections and operators

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -39,3 +39,98 @@ print("I live in \(address.street), \(address.number) \(address.zipCode)")
 let (street, number, zipCode) = address
 
 //Optional
+var licenseNumber: String?
+
+//Unwrap
+print("My license number is \(licenseNumber!)")
+
+//Optional bind
+if let licenseNumber = licenseNumber {
+    print("My license number is \(licenseNumber)")
+} else {
+    print("No License number")
+}
+
+//Nil coalescing operator
+let myLicenseNumber = licenseNumber ?? ""
+
+//Collections
+//Array
+var fruits: [String] = ["Banana", "Strawberry", "Watermelon"]
+var emptyArray: [String] = []
+
+fruits.isEmpty
+fruits.append("Apple")
+fruits.insert("Avocado", at: 0)
+fruits.firstIndex(of: "Banana")
+fruits.contains("Strawberry")
+
+//Total elements
+print(fruits.count)
+
+//Dictionary
+var states: [String: String] = [
+    "SP": "São Paulo",
+    "RJ": "Rio de Janeiro",
+    "PA": "Pará",
+    "PI": "Piauí"
+]
+
+var emptyStates: [String: String] = [:]
+
+states.count
+states.isEmpty
+states["AC"] = "Acre"
+states["AC"] = nil
+
+//Set
+var movies: Set<String> = ["Spider Man", "The Avengers", "Thor: Ragnarok", "Hulk"]
+var emptyMovies: Set<String> = []
+
+movies.insert("Ironman")
+movies.count
+
+var toBeWatched: Set<String> = ["Doctor Strange", "Black Panther"]
+let intersectionMovies = movies.intersection(toBeWatched)
+let unionMovies = movies.union(toBeWatched)
+
+//Prefix
+prefix operator >-
+prefix func >- (rhs: Int) -> Int {
+    return rhs*rhs
+}
+
+>-8
+
+//Postfix
+postfix operator -<
+postfix func -< (lhs: Int) -> Int {
+    return lhs*lhs
+}
+
+7-<
+
+//Infix
+infix operator >-<
+func >-< (rhs: Int, lhs: Int) -> Int {
+    return 2 * lhs * rhs
+}
+
+5 >-< 9
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
